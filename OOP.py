4e9db901e8eb709bbb15c03c29865d8b0c4edbc8
Guest <guest-ykWTj2@meet.mit.edu>
2015-01-15 20:02:53 +0200

    Finished Zoo

---
+++ OOP.py
@@ -0,0 +1,12 @@
+class Animal:
+	def __init__ (self,name,age,color,size):
+		self.name=name
+		self.age=age
+		self.color=color
+		self.size=size 
+		
+
+dog=Animal("nofar",15,"pink","tiny")
+
+print(dog.size)
+		
